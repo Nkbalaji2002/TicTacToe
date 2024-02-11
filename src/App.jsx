f67c78430ea5fb8f3297989947ea81ac8061ffc1
Nkbalaji2002 <nithishkumarbsc@gmail.com>
2024-02-11 12:16:43 +0530

    Start Star_Rating

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import RandomColor from "./components/Random_Color";
+import Star_Rating from "./components/Star_Rating";
 
 const App = () => {
   return (
     <>
       {/* <h1>Hello World from React</h1> */}
       {/* <Accordian /> */}
-      <RandomColor />
+      {/* <RandomColor /> */}
+      <Star_Rating />
     </>
   );
 };
---
+++ src/components/Star_Rating/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Star_Rating = () => {
+  return <div>Star_Rating</div>;
+};
+
+export default Star_Rating;
