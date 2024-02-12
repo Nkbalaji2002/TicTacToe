a1a2f01ceb34c5b9f5c714e4cff048363ce234f3
Nkbalaji2002 <nithishkumarbsc@gmail.com>
2024-02-12 06:50:52 +0530

    remove comments

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,6 @@ import CustomTabs from "./components/TabsCustom";
 const App = () => {
   return (
     <>
-      {/* <h1>Hello World from React</h1> */}
-      {/* <Accordian /> */}
-      {/* <RandomColor /> */}
-      {/* <Star_Rating /> */}
-      {/* <DarkTheme /> */}
       <CustomTabs />
     </>
   );
