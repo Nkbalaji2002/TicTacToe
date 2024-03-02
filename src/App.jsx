9bf3a2b47330ec8065efc2828710f97adb0d4625
Nkbalaji2002 <nithishkumarbsc@gmail.com>
2024-03-02 12:52:28 +0530

    Tic-Tac-Toe commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import "./index.css";
 
 import "primeicons/primeicons.css";
-import TreeView from "./components/TreeView";
-import { menus } from "./components/TreeView/data";
+import TicTacToeApp from "./components/Tic-Tac-Toe";
 
 const App = () => {
   return (
     <>
       {/* <CustomTabs /> */}
-      <TreeView menus={menus} />
+      {/* <TreeView menus={menus} /> */}
+      <TicTacToeApp />
     </>
   );
 };
